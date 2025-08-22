1369ac33b8bcea5a47fd488b385efabfbfbcb911
K3VlNN <fonsecakevin112@gmail.com>
2025-08-22 11:25:22 -0500

    m

--- proyecto_react4/src/card.jsx
+++ proyecto_react4/src/card.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
+  import { useState } from 'react';
 
 export default function Card() {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(true);
   const toggleDarkMode = () => setDarkMode(!darkMode);
 
   return (
@@ -13,24 +13,16 @@ export default function Card() {
       <h2 className="mt-4 text-2xl font-semibold text-center">
         Ramon Guitierres Soler
       </h2>
-      <img
-        src="https://i.pravatar.cc/150?img=3"
-        alt="Perfil"
-        className="rounded-full w-32 mx-auto"
-      />
+      <img src="https://i.pravatar.cc/150?img=3"alt="Perfil"className="rounded-full w-32 mx-auto"/>
       <p className="mt-2 text-center">
         Desarrollador Front-end apasionado por React y el diseño UI/UX.
       </p>
 
       {/* ✅ Botón centrado debajo del texto */}
       <button
-        onClick={toggleDarkMode}
-        className={`mt-6 px-4 py-2 rounded block mx-auto ${
+        onClick={toggleDarkMode}className={`mt-6 px-4 py-2 rounded block mx-auto ${
           darkMode ? 'bg-blue-500 text-white' : 'bg-gray-600 text-white'
-        }`}
-      >
-        Cambiar Fondo
-      </button>
+          }`}>Cambiar Fondo</button>
     </div>
   );
 }
